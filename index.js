411f37e4848c158069d52b2458c3d61ef898e996
Sebastian Caicedo <sdrivert@hotmail.com>
2024-04-13 15:40:54 -0500

    OCR feature added

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const vision = require('@google-cloud/vision')
 const path = require('path');
+const express = require('express');
+const multer = require('multer');
+
+const app = express()
+const port = 3000
+
+//Dependencia que sirve para subir archivos en express, guarda todo en /uploads.
+const upload = multer({
+    dest: 'uploads/'  // This is the folder where files will be saved temporarily
+});
 
 
+app.set('views', './views'); // Define la carpeta donde se guardarán tus archivos Pug
+app.set('view engine', 'pug'); // Establece Pug como el motor de plantillas
+
+app.listen(port, ()=>
+{
+    console.log("Server is running  on port ", port)
+})
+
+// Importando el archivo de configuracion de google API Vision.
 const keyFilename = path.join(__dirname, 'uniperruno.json');
 
-quickStart()
-async function quickStart(){
-    const client = new vision.ImageAnnotatorClient({keyFilename})
-    // Para detectar texto de las imagenes.
-    const [result] = await client.labelDetection('./resources/gato.jpg');
+app.get("/", (req, res)=>{
+    res.render("upload")
+})
 
-    // const [result] = await client.textDetection('./resources/gato.jpg');
-    // Para 
-    const labels = result.labelAnnotations
-    labels.forEach(label => console.log(label.description))
+app.post("/upload", upload.single('image'), (req, res) =>{
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    const filePath = path.join(__dirname, req.file.path);
+
+    const text = opticalRecognition(filePath)
+    
+})
+
+const client = new vision.ImageAnnotatorClient({ keyFilename });
+
+async function opticalRecognition(imagePath){
+    const [result] = await client.textDetection(imagePath);
+    const detections = result.textAnnotations;
+    return detections.at(0).description
 }
 
 
